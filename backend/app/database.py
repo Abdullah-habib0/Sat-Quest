5c774bc820775a50c82b2e7b808b8a90d6180b3a
Abdullah Habib <abdullah.habib7575@gmail.com>
2026-06-11 22:53:18 +0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,23 +1,25 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import settings
+import certifi
 
 client: AsyncIOMotorClient = None
 db = None
 
-
 async def connect_db():
     global client, db
-    client = AsyncIOMotorClient(settings.MONGO_URI)
+    client = AsyncIOMotorClient(
+        settings.MONGO_URI,
+        tls=True,
+        tlsCAFile=certifi.where()
+    )
     db = client[settings.DB_NAME]
     print("Connected to MongoDB successfully")
-
 
 async def close_db():
     global client
     if client:
         client.close()
         print("MongoDB connection closed")
-
 
 def get_db():
     return db
